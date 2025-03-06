383cead39195ac7563faa8cbef566dec6387940b
gregrodz32 <111553700+gregrodz32@users.noreply.github.com>
2025-03-06 12:46:06 -0600

    update logger.j

--- java/Logger.java
+++ java/Logger.java
@@ -1,2 +1,23 @@
+import java.io.FileWriter;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+
 public class Logger { 
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("java logger <log file>");
+            return;
+        }
+        String logFile = args[0];
+
+        try (PrintWriter logWriter = new PrintWriter(new FileWriter(logFile, true)))
+            Scanner scanner = new scanner(System.in)) {
+
+
+
+            }
+    }
 }
