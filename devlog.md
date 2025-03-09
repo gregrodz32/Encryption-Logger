082453e8f2cf7602a1204f960e1e4f2ee162cd25
gregrodz32 <111553700+gregrodz32@users.noreply.github.com>
2025-03-08 22:02:20 -0600

    Completed the logger.java file and tested successfully in .txt

--- devlog.md
+++ devlog.md
@@ -8,3 +8,9 @@ Will be working with Java and was provided a mainmemory.java and CPU.java file
 The driver will use pipes to communicate with the other programes, there will be interprocess communication between the three projects
 
 I will start by creating the Logger.java file 
+
+3/8/25 - Finish Logger file
+
+Tested logger.java to create a log.txt file and passed 
+
+starting on the encrypting file
--- java/Logger.java
+++ java/Logger.java
@@ -1,23 +1,31 @@
-import java.io.FileWriter;
 import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
-
-public class Logger { 
+public class Logger {
     public static void main(String[] args) {
         if (args.length != 1) {
-            System.out.println("java logger <log file>");
+            System.out.println("Usage: java Logger <logfile>");
             return;
         }
+
         String logFile = args[0];
 
-        try (PrintWriter logWriter = new PrintWriter(new FileWriter(logFile, true)))
-            Scanner scanner = new scanner(System.in)) {
+        try (PrintWriter logWriter = new PrintWriter(new FileWriter(logFile, true));
+             Scanner scanner = new Scanner(System.in)) {
 
+            while (scanner.hasNextLine()) {
+                String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("QUIT")) break;
 
-
+                String timestamp = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(new Date());
+                logWriter.println(timestamp + " " + input);
+                logWriter.flush();
             }
+
+        } catch (IOException e) {
+            System.out.println("Error writing to log file: " + e.getMessage());
+        }
     }
 }
